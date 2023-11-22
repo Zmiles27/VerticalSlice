6456057ec3623c63fce4a23c2b9901ff37adb187
Tenseven-107 <nadrichem@tanous.nl>
2023-11-22 11:56:02 +0100

    Added a jump and groundcheck

--- Mario3DWorldVerticalSlice/Assets/Scripts/Level Elements/PickupHolder.cs
+++ Mario3DWorldVerticalSlice/Assets/Scripts/Level Elements/PickupHolder.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PickupHolder : MonoBehaviour
 {
-    public int coin = 0;
-    public int score = 0;
+    // Be sure to add the "PickupManager" tag to the object this script is on!
+
+    public int coin = 0; // Current number of collected coins
+    public int score = 0; // Current number of score
 
 
+    // Add score and coins
     public void AddValues(int newCoins, int newScore)
     {
         coin += newCoins;
--- Mario3DWorldVerticalSlice/Assets/Scripts/Testing_noah/TestMovement.cs
+++ Mario3DWorldVerticalSlice/Assets/Scripts/Testing_noah/TestMovement.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class TestMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
-    Vector3 velocity = Vector3.zero;
+    [SerializeField] float speed = 5f; // Move speed
+    Vector3 velocity = Vector3.zero; // Velocity
 
-    Rigidbody rb;
+    Rigidbody rb; // The rigidbody
 
 
     private void Start()
@@ -17,12 +17,17 @@ public class TestMovement : MonoBehaviour
     }
 
 
+    // Movement
     private void FixedUpdate()
     {
+        // Get the input
         float zInput = Input.GetAxisRaw("Vertical");
         float xInput = Input.GetAxisRaw("Horizontal");
+
+        // Velocity is the input vector times the movement speed.
         velocity = new Vector3(xInput, 0, zInput) * speed * Time.deltaTime;
 
+        // If the velocity is not Vector2(0, 0), then the player is allowed to move.
         if (velocity != Vector3.zero)
         {
             rb.MovePosition(transform.position + velocity);
